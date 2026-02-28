f16d15740ab48632ce86298e877734d268a04f96
anti-hero-amir <a.amiiir1297@gmail.com>
2026-02-28 06:14:46 +0200

    Fix flake8 style issue

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"message": "CI Professional Project"}
+
 
 @app.get("/add/{a}/{b}")
 def add(a: int, b: int):
